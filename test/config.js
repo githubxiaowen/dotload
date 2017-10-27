8618d549b85434def0f48b803ab1a353bfac8ca1
dolby <dolby.xiaowen@gmail.com>
2017-10-27 10:31:16 +0800

    add test

--- test/config.js
+++ test/config.js
@@ -1,1 +1,8 @@
 
+module.exports = {
+  hostname: 'localhost', //host ip
+  port: 9000, // port
+  method: 'POST',
+  path: '/upload',
+  to: '/Users/didi/work/dotload/target/'  // target static folder;absolute path recommend
+}
